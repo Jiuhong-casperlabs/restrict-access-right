553269aaaecbbea760f58cf1de7885b9841151e3
Jiuhong <jiuhong@casperlabs.io>
2022-06-28 15:48:59 +0800

    remove useless

--- tests/src/integration_tests.rs
+++ tests/src/integration_tests.rs
@@ -2,11 +2,12 @@
 mod tests {
 
     use casper_engine_test_support::{
-        ExecuteRequestBuilder, InMemoryWasmTestBuilder, DEFAULT_ACCOUNT_ADDR,
+        ExecuteRequestBuilder, InMemoryWasmTestBuilder, WasmTestBuilder, DEFAULT_ACCOUNT_ADDR,
         DEFAULT_RUN_GENESIS_REQUEST,
     };
-    use casper_execution_engine::core::{
-        engine_state::Error as CoreError, execution::Error as ExecError,
+    use casper_execution_engine::{
+        core::{engine_state::Error as CoreError, execution::Error as ExecError},
+        storage::global_state::in_memory::InMemoryGlobalState,
     };
     use casper_types::{runtime_args, system::mint, ApiError, ContractHash, RuntimeArgs, U512};
 
@@ -14,10 +15,7 @@ mod tests {
     const GOOD_WASM: &str = "good.wasm";
     const BAD_WASM: &str = "bad.wasm";
 
-    #[test]
-    fn good() {
-        let mut builder = InMemoryWasmTestBuilder::default();
-
+    fn deploy_market_contract(builder: &mut WasmTestBuilder<InMemoryGlobalState>) -> ContractHash {
         let exec_request_1 =
             ExecuteRequestBuilder::standard(*DEFAULT_ACCOUNT_ADDR, CONTRACT_WASM, runtime_args! {})
                 .build();
@@ -38,6 +36,14 @@ mod tests {
             .into_hash()
             .map(ContractHash::new)
             .expect("should be contracthash");
+
+        contract_hash
+    }
+    #[test]
+    fn good() {
+        let mut builder = InMemoryWasmTestBuilder::default();
+
+        let contract_hash = deploy_market_contract(&mut builder);
 
         let exec_request_2 = ExecuteRequestBuilder::standard(
             *DEFAULT_ACCOUNT_ADDR,
@@ -56,26 +62,7 @@ mod tests {
     fn bad() {
         let mut builder = InMemoryWasmTestBuilder::default();
 
-        let exec_request_1 =
-            ExecuteRequestBuilder::standard(*DEFAULT_ACCOUNT_ADDR, CONTRACT_WASM, runtime_args! {})
-                .build();
-
-        builder
-            .run_genesis(&DEFAULT_RUN_GENESIS_REQUEST)
-            .exec(exec_request_1)
-            .expect_success()
-            .commit();
-        let account = builder
-            .get_account(*DEFAULT_ACCOUNT_ADDR)
-            .expect("should have account");
-
-        let contract_hash = account
-            .named_keys()
-            .get("contract1")
-            .expect("should have contract1")
-            .into_hash()
-            .map(ContractHash::new)
-            .expect("should be contracthash");
+        let contract_hash = deploy_market_contract(&mut builder);
 
         let exec_request_2 = ExecuteRequestBuilder::standard(
             *DEFAULT_ACCOUNT_ADDR,
